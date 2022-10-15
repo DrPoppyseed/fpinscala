1d3921854fd41a7e8993727901e91a6ee4b165f4
DrPoppyseed <peaske16180@gmail.com>
2022-10-15 15:31:24 +0900

    Up till 3.23

--- src/main/scala/datastructures/List.scala
+++ src/main/scala/datastructures/List.scala
@@ -18,7 +18,7 @@ object List:
 
   def apply[A](as: A*): List[A] =
     if as.isEmpty then Nil
-    else Cons(as.head, apply(as.tail*))
+    else Cons(as.head, apply(as.tail *))
 
   def tail[A](as: List[A]): List[A] = as match
     case Nil => Nil
@@ -81,10 +81,41 @@ object List:
     foldLeft(as, List[A](), (acc, h) => Cons(h, acc))
 
   def foldRightViaFoldLeft[A, B](as: List[A], acc: B, f: (A, B) => B): B =
-    foldLeft(as, acc, (a, b) => f(b, a))
+    foldLeft(reverseViaFoldLeft(as), acc, (a, b) => f(b, a))
 
   def appendViaFoldLeft[A](a1: List[A], a2: List[A]): List[A] =
     foldLeft(reverseViaFoldLeft(a1), a2, (acc, h) => Cons(h, acc))
 
   def appendViaFoldRight[A](a1: List[A], a2: List[A]): List[A] =
-    foldRight(a1, a2, Cons(_,_))
+    foldRight(a1, a2, Cons(_, _))
+
+  def concat[A](as: List[List[A]]): List[A] =
+    foldRight(as, Nil: List[A], appendViaFoldRight)
+
+  def mapAddOne(l: List[Int]): List[Int] =
+    foldRightViaFoldLeft(l, Nil: List[Int], (h, t) => Cons(h + 1, t))
+
+  def mapParseDoubleToString(l: List[Double]): List[String] =
+    foldRightViaFoldLeft(l, Nil: List[String], (h, t) => Cons(h.toString, t))
+
+  def map[A, B](as: List[A], f: A => B): List[B] =
+    foldRightViaFoldLeft(as, Nil: List[B], (h, t) => Cons(f(h), t))
+
+  def filter[A](as: List[A], f: A => Boolean): List[A] =
+    foldRightViaFoldLeft(as, Nil: List[A], (h, t) => if (f(h)) Cons(h, t) else t)
+
+  def flatMap[A, B](as: List[A], f: A => List[B]): List[B] =
+    foldLeft(as, Nil: List[B], (t, h) => append(t, f(h)))
+
+  def filterViaFlatMap[A](as: List[A], f: A => Boolean): List[A] =
+    flatMap(as, h => if (f(h)) List(h) else List())
+
+  def zipInts(l: List[Int], r: List[Int]): List[Int] = (l, r) match
+    case (_, Nil) => Nil
+    case (Nil, _) => Nil
+    case (Cons(hl, tl), Cons(hr, tr)) => Cons(hl + hr, zipInts(tl, tr))
+
+  def zipWith[A, B, C](l: List[A], r: List[B], f: (A, B) => C): List[C] = (l, r) match
+    case (_, Nil) => Nil
+    case (Nil, _) => Nil
+    case (Cons(hl, tl), Cons(hr, tr)) => Cons(f(hl, hr), zipWith(tl, tr, f))
